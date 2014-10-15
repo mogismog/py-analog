0c9542e86d7f40a69aefea4c36bcbb64b458bbae
Francisco Alvarez <falvare6@slu.edu>
2014-10-15 12:20:24 -0600

    New functions, combined classes
    Made a couple new numba functions, combined what I wanted to be two
    separated classes into one class that makes it easier to set
    training/forecast data.

--- py-analog/analog/analog.py
+++ py-analog/analog/analog.py
@@ -8,20 +8,15 @@ from utils import find_nearest_idx
 
 from comp_funcs import *
 
-class SFAnalog(object):
+class Analog(object):
     """Various methods to produce a single deterministic/probabilistic forecast via analog method."""
 
-    def __init__(self, forecast, train, grid_window=3, comp_method=['Rank'], field_weights=[],
-                 lat_bounds=[], lon_bounds=[], forecast_lats=[], forecast_lons=[], lat_inc=1., lon_inc=1.):
+    def __init__(self,grid_window=3, comp_method=['Rank'], field_weights=[],
+                 lat_bounds=[], lon_bounds=[], forecast_lats=[], forecast_lons=[], lat_inc=1., lon_inc=1.,
+                 forecast_dates=[]):
         """
         Initialize the analog object.
 
-        :param forecast:
-            NumPy array, Either a 2-d (lat,lon) or 3-d (n_vars,lat,lon) numpy array of "current" forecast data.
-        :param train:
-            NumPy array, Either a 3-d (time,lat,lon) or 4-d (n_vars,time,lat,lon) numpy array of "past"/training data.
-        :param forecast_date:
-            datetime object, the forecast date in question.
         :param grid_window:
             integer, +/- number of grid points (n/s, e/w) around each forecast grid point
             to include in local domain pattern matching.
@@ -41,28 +36,13 @@ class SFAnalog(object):
         :param lat_inc/lon_inc:
             float
             The change in latitude/longitude each grid point (e.g. dx or dy).
+        :param forecast_dates:
+            float
+            The change in latitude/longitude each grid point (e.g. dx or dy).
         :return: self
         """
-        # --- Set up the class attributes
-        self.forecast = forecast
-        self.train = train
-        self.forecast_date = forecast_date
 
-        # --- Here, we check to make sure everything is copacetic.
-        if (len(self.forecast.shape) > 2) and (self.forecast.shape[0] > 1):
-            self.n_vars = self.forecast.shape[0]
-            # --- First, make sure we're dealing with same number of vars between fcst/train
-            if self.forecast.shape[0] != self.train.shape[0]:
-                raise ValueError("Different number of variables between forecast and training data arrays!")
-            # --- Make sure there aren't more field_weights than variables
-            if len(field_weights) > self.forecast.shape[0]:
-                raise ValueError("More field_weights than variables!")
-            if len(comp_method) > self.forecast.shape[0]:
-                raise ValueError("More methods than variables!")
-        else:
-            self.n_vars = 1
-
-        # --- Now, to make sure the method selected is available, and if it is then add
+        # --- Here, we make sure the method(s) selected is/are available, and if it/they is/are then add
         available_methods = ['rank','rmse','mae','corr',]
         self.comp_method = []
         for mthd in comp_method:
@@ -77,6 +57,7 @@ class SFAnalog(object):
         self.field_weights = field_weights
 
         # --- Here, we check to make sure everything is copacetic with the domain specifications
+        # ---
         if len(forecast_lats) != len(forecast_lons):
             raise ValueError("len(lat_bounds) != len(lon_bounds)\nWe either need to forecast for a point or domain.")
 
@@ -129,10 +110,47 @@ class SFAnalog(object):
         self.lat_inc = lat_inc
         self.lon_inc = lon_inc
 
+
     def __repr__(self):
         return "<Analog(grid_window={}, comp_method={}, field_weights={},lat_bounds={}, lon_bounds={}, forecast_lats={}, forecast_lons={}, lat_inc={}, lon_inc={})>".format(
                              self.grid_window,self.comp_method, self.field_weights, self.lat_bounds,
                              self.lon_bounds, self.forecast_lats, self.forecast_lons, self.lat_inc, self.lon_inc)
+
+
+    def __str__(self):
+        return "Analog(grid_window={}, comp_method={}, field_weights={},lat_bounds={}, lon_bounds={}, forecast_lats={}, forecast_lons={}, lat_inc={}, lon_inc={})".format(
+                             self.grid_window,self.comp_method, self.field_weights, self.lat_bounds,
+                             self.lon_bounds, self.forecast_lats, self.forecast_lons, self.lat_inc, self.lon_inc)
+
+    def analog_data(self,train,forecast):
+
+        """
+        Initialize the training and forecast data with which to find analogs.
+
+        :param forecast:
+            NumPy array, Either a 2-d (lat,lon) or 3-d (n_vars,lat,lon) numpy array of "current" forecast data.
+        :param train:
+            NumPy array, Either a 3-d (time,lat,lon) or 4-d (n_vars,time,lat,lon) numpy array of "past"/training data.
+        :return: self
+        """
+        # --- Here, we check to make sure everything is copacetic between the forecast/train array shapes
+        # --- and other pre-defined variables
+        if (len(forecast.shape) > 2) and (forecast.shape[0] > 1):
+            self.n_vars = forecast.shape[0]
+            # --- First, make sure we're dealing with same number of vars between fcst/train
+            if forecast.shape[0] != train.shape[0]:
+                raise ValueError("Different number of variables between forecast and training data arrays!")
+            # --- Make sure there aren't more field_weights than variables
+            if len(field_weights) > self.forecast.shape[0]:
+                raise ValueError("More field_weights than variables!")
+            if len(comp_method) > self.forecast.shape[0]:
+                raise ValueError("More methods than variables!")
+        else:
+            self.n_vars = 1
+
+        # --- Set up the class attributes
+        self.forecast = forecast
+        self.train = train
 
 
     def find_analogs(self):
@@ -148,118 +166,37 @@ class SFAnalog(object):
             analog_idxs = np.ones(self.train.shape[0]) * -9999.9
         else:
             # --- Make analog indices array first, then fill it in
-            if self.n_vars == 1:
-                analog_idxs = np.ones(self.train.shape) * -9999.9
-            else:
-                analog_idxs = np.ones((self.train.shape[0], self.train.shape[2], self.train.shape[3])) * -9999.9
+            analog_idxs = np.ones(self.train.shape) * -9999.9
 
-            # --- Now, find closest analogs...
+        # --- Now, let's find the closest analogs
+        if self.n_vars == 1: # --- Only doing a single field
+            if self.comp_method[0] == 'rank':
+                _rank_analog_grid(self.train,self.forecast,analog_idxs,self.start_lat_idx,self.stop_lat_idx,
+                                  self.start_lon_idx,self.stop_lon_idx, self.grid_window)
+            elif self.comp_method[0] == 'rmse':
+                _rmse_analog_grid(self.train,self.forecast,analog_idxs,self.start_lat_idx,self.stop_lat_idx,
+                                  self.start_lon_idx,self.stop_lon_idx, self.grid_window)
+            elif self.comp_method[0] == 'mae':
+                _mae_analog_grid(self.train,self.forecast,analog_idxs,self.start_lat_idx,self.stop_lat_idx,
+                                  self.start_lon_idx,self.stop_lon_idx, self.grid_window)
+        elif self.n_vars > 1:
+            for meth, nvar in enumerate(self.comp_method):
+                if meth == 'rank':
+                    _rank_analog_grid(self.train[nvar,...],self.forecast[nvar,...],analog_idxs[nvar,...],self.start_lat_idx,self.stop_lat_idx,
+                                      self.start_lon_idx,self.stop_lon_idx, self.grid_window)
+                elif meth == 'rmse':
+                    _rmse_analog_grid(self.train[nvar,...],self.forecast[nvar,...],analog_idxs[nvar,...],self.start_lat_idx,self.stop_lat_idx,
+                                      self.start_lon_idx,self.stop_lon_idx, self.grid_window)
+                elif meth == 'mae':
+                    _mae_analog_grid(self.train[nvar,...],self.forecast[nvar,...],analog_idxs[nvar,...],self.start_lat_idx,self.stop_lat_idx,
+                                      self.start_lon_idx,self.stop_lon_idx, self.grid_window)
 
-        # --- Ok, let's do this...
-        if self.point_fcst:
-            blah = 0
-            # --- We then find the closest analogs
+                # --- Now, we add weights to the distances...
+                analog_idxs[nvar,...] *= self.field_weights[nvar]
 
-class MFAnalog(object):
-    """Various methods to produce multiple deterministic/probabilistic forecasts via analog method."""
+            # --- sum up distances along variable axis
+            analog_idxs = np.sum(analog_idxs,axis=0)
 
-    def __init__(self, forecast, train, ):
-        """
-        Initialize the analog object.
-
-        :param forecast:
-            NumPy array, Either a 2-d (lat,lon) or 3-d (n_vars,lat,lon) numpy array of "current" forecast data.
-        :param train:
-            NumPy array, Either a 3-d (time,lat,lon) or 4-d (n_vars,time,lat,lon) numpy array of "past"/training data.
-        :return:
-        """
-        # --- Set up the class attributes
-        self.forecast = forecast
-        self.train = train
-
-
-    def analog_params(self, grid_window=3, comp_method=['Rank'], field_weights=[]):
-        """
-        Set up some parameters for whatever analog method.
-
-        Parameters
-        ----------
-        grid_window : integer
-            +/- number of grid points (n/s, e/w) around each forecast grid point to include in local domain pattern\n
-            matching. For example, if grid_window = 3, we use 49 grid points to calculate differences between forecast/training\n
-            data (3*2 + 1)*(3+2 +1).
-        comp_method : list
-            Method with which to pattern match. Options: ['Rank','MAE','RMSE','Corr'].
-            if n_vars > 1, can use different methods to compare each variable.
-        """
-
-        # --- Here, we check to make sure everything is copacetic.
-        if (len(self.forecast.shape) > 2) and (self.forecast.shape[0] > 1):
-            self.n_vars = self.forecast.shape[0]
-            # --- First, make sure we're dealing with same number of vars between fcst/train
-            if self.forecast.shape[0] != self.train.shape[0]:
-                raise ValueError("Different number of variables between forecast and training data arrays!")
-            # --- Make sure there aren't more field_weights than variables
-            if len(field_weights) > self.forecast.shape[0]:
-                raise ValueError("More field_weights than variables!")
-            if len(comp_method) > self.forecast.shape[0]:
-                raise ValueError("More methods than variables!")
-        else:
-            self.n_vars = 1
-
-        # --- Now, to make sure the method(2) selected is available, and if it is then add
-        available_methods = ['rank','rmse','mae','corr']
-        self.comp_method = []
-        for mthd in comp_method:
-            if any(mthd.lower() in avail for avail in available_methods):
-                self.comp_method.append(mthd.lower())
-            else:
-                warnings.warn('Method {} not an option! Reverting to rank method'.format(mthd.lower()))
-                self.comp_method.append('rank')
-
-
-
-        # --- Everything cool? Ok, let's do this.
-        self.grid_window = grid_window
-        self.weights = field_weights
-
-        return self
-
-    def domain(self, lat_bounds=[], lon_bounds=[], forecast_lats=[], forecast_lons=[], lat_inc=1, lon_inc=1):
-        """
-        Main and forecast domain setup.
-
-        Parameters
-        ----------
-        lat_bounds/lon_bounds : list or NumPy array
-            A list/numpy array of all latitude within the forecast and training data.\n
-            len(allLats) = forecastData[0] or forecastData[1] if forecastData.shape == 3.
-        forecast_last/forecast_lons : list or NumPy array
-            Either a list/numpy array of min/max forecast lats/lons (defining a domain) or a single point.
-        lat_inc/lon_inc : float
-            The change in latitude/longitude each grid point (e.g. dx or dy).
-        """
-        # --- Here, we check to make sure everything is copacetic.
-        if len(forecast_lats) != len(forecast_lons):
-            raise ValueError("len(lat_bounds) != len(lon_bounds)\nWe either need to forecast for a point or domain.")
-
-        # --- Everything cool? Ok, let's do this.
-        if len(forecast_lats) == 1:
-            self.point_fcst = True
-        else:
-            self.point_fcst = False
-
-        self.allLats = np.arange(lat_bounds[0], lat_bounds[-1] + 1, lat_inc)
-        self.allLons = np.arange(lon_bounds[0], lon_bounds[-1] + 1, lon_inc)
-        # --- If we're dealing with a point forecast, we will find the closest lat/lon grid points to the fcst point.
-        if len(forecast_lats) > 1:
-            self.closest_lat = self.allLats[find_nearest_idx(self.allLats[:], self.fcstLats[0])[0]]
-            self.closest_lon = self.allLons[find_nearest_idx(self.allLons[:], self.fcstLons[0])[0]]
-        else:
-            self.fcstLats = forecast_lats
-            self.fcstLons = forecast_lons
-
-        return self
-
-
-
+        # --- now find indices of closest ranks
+        self.analogs = argsort_analogs(analog_idxs,elf.start_lat_idx,self.stop_lat_idx,
+                                      self.start_lon_idx,self.stop_lon_idx)
--- py-analog/analog/comp_funcs.py
+++ py-analog/analog/comp_funcs.py
@@ -5,62 +5,79 @@ from scipy.stats import rankdata as rd
 from numba import autojit
 
 @autojit
-def _rank_analog_grid(in_array,out_array,i_start,i_stop,j_start,j_stop, grid_window):
+def _rank_analog_grid(train,forecast,out_array,i_start,i_stop,j_start,j_stop, grid_window):
     """
     Function to find analogous dates based on root mean square error.
     :param array:
         Some 2-d numpy array
     :return self:
     """
-    shp1 = (in_array.shape[0]-1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    shp1 = (train.shape[0],(((grid_window*2)+1)*((grid_window*2)+1)))
     shp2 = (1,(((grid_window*2)+1)*((grid_window*2)+1)))
-    ranked_array = np.ones((in_array.shape))*-9999
+    ranked_array = np.empty((train.shape[0]+1,forecast.shape[0],forecast.shape[1]))
+    # --- Fill in ranked array
+    ranked_array[:-1,...] = train
+    ranked_array[-1,...] = forecast
     # --- First, rank data at each array in relevant domain
     for i in range(i_start-grid_window,i_stop+grid_window+1,1): # --- latitudes
         for j in range(j_start-grid_window,j_stop+grid_window+1,1): # --- longitudes
-            ranked_array[:,i,j] = rd(in_array[:,i,j],method='average')
+            ranked_array[:,i,j] = rd(ranked_array[:,i,j],method='average')
     # --- Now find differences at each forecast grid point and local-domain
     for i in range(i_start,i_stop+1,1): # --- latitudes
         for j in range(j_start,j_stop+1,1): # --- longitudes
             #temp_array[:,:] = in_array[:,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(temp_array.shape)
-            out_array[:,i,j] = np.argsort(np.sum(ranked_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
+            out_array[:,i,j] = (np.sum(ranked_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
                                       -ranked_array[-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2),
                                       axis=1,dtype=np.int))
     return out_array
 
 
 @autojit
-def _rmse_analog_grid(in_array,out_array,i_start,i_stop,j_start,j_stop, grid_window):
+def _rmse_analog_grid(train,forecast,out_array,i_start,i_stop,j_start,j_stop, grid_window):
     """
     Function to find analogous dates based on root mean square error.
     :param array:
         Some 2-d numpy array
     :return self:
     """
-    shp1 = (in_array.shape[0]-1,(((grid_window*2)+1)*((grid_window*2)+1)))
-    shp2 = (1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    shp1 = (train.shape[0],(((grid_window*2)+1)*((grid_window*2)+1)))
+    shp2 = ((((grid_window*2)+1)*((grid_window*2)+1)))
     # --- Now find differences at each forecast grid point and local-domain
     for i in range(i_start,i_stop+1,1): # --- latitudes
         for j in range(j_start,j_stop+1,1): # --- longitudes
-            out_array[:,i,j] = np.argsort(np.sqrt(np.mean((in_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
-                                      -in_array[-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2))**2,
+            out_array[:,i,j] = (np.sqrt(np.mean((train[:,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
+                                      -forecast[i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2))**2,
                                       axis=1,dtype=np.float),dtype=np.float))
     return out_array
 
 @autojit
-def _mae_analog_grid(in_array,out_array,i_start,i_stop,j_start,j_stop, grid_window):
+def _mae_analog_grid(train,forecast,out_array,i_start,i_stop,j_start,j_stop, grid_window):
     """
     Function to find analogous dates based on root mean square error.
     :param array:
         Some 2-d numpy array
     :return self:
     """
-    shp1 = (in_array.shape[0]-1,(((grid_window*2)+1)*((grid_window*2)+1)))
-    shp2 = (1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    shp1 = (train.shape[0],(((grid_window*2)+1)*((grid_window*2)+1)))
+    shp2 = ((((grid_window*2)+1)*((grid_window*2)+1)))
     # --- Now find differences at each forecast grid point and local-domain
     for i in range(i_start,i_stop+1,1): # --- latitudes
         for j in range(j_start,j_stop+1,1): # --- longitudes
-            out_array[:,i,j] = np.argsort(np.mean(np.abs(in_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
-                                      -in_array[-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2)),
+            out_array[:,i,j] = (np.mean(np.abs(train[:,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
+                                      -forecast[i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2)),
                                       axis=1,dtype=np.float))
     return out_array
+
+@autojit
+def argsort_analogs(analog_array,i_start,i_stop,j_start,j_stop):
+    """
+    Function to find analogous dates based on root mean square error.
+    :param array:
+        Some 2-d numpy array
+    :return self:
+    """
+    # --- Now find differences at each forecast grid point and local-domain
+    for i in range(i_start,i_stop+1,1): # --- latitudes
+        for j in range(j_start,j_stop+1,1): # --- longitudes
+            analog_array[:,i,j] = np.argsort(analog_array[:,i,j])
+    return analog_array
