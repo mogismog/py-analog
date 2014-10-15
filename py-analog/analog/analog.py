05701d28a9c3f06074ce1bc6a82d460daf881db1
Francisco Alvarez <falvare6@slu.edu>
2014-10-15 09:39:58 -0600

    More updates
    Got the numba autojit stuff working (yay!), moved utils.py to a better
    location, fixed up some of the functions.

--- py-analog/analog/analog.py
+++ py-analog/analog/analog.py
@@ -4,7 +4,9 @@ import warnings
 
 import numpy as np
 
-from pyanalog.utils import find_nearest_idx
+from utils import find_nearest_idx
+
+from comp_funcs import *
 
 class SFAnalog(object):
     """Various methods to produce a single deterministic/probabilistic forecast via analog method."""
@@ -27,7 +29,7 @@ class SFAnalog(object):
             to calculate differences between forecast/training
             data (3*2 + 1)*(3+2 +1).
         :param comp_method
-            list, Method with which to pattern match. Options: ['Rank','MAE','RMSE','Corr', 'MSE'].
+            list, Method with which to pattern match. Options: ['Rank','MAE','RMSE',].
             if n_vars > 1, can use different methods to compare each variable.
         :param lat_bounds/lon_bounds:
             list or NumPy array
@@ -61,7 +63,7 @@ class SFAnalog(object):
             self.n_vars = 1
 
         # --- Now, to make sure the method selected is available, and if it is then add
-        available_methods = ['rank','rmse','mae','corr','mse']
+        available_methods = ['rank','rmse','mae','corr',]
         self.comp_method = []
         for mthd in comp_method:
             if any(mthd.lower() in avail for avail in available_methods):
@@ -87,13 +89,39 @@ class SFAnalog(object):
         self.all_lats = np.arange(lat_bounds[0], lat_bounds[-1] + 1, lat_inc)
         self.all_lons = np.arange(lon_bounds[0], lon_bounds[-1] + 1, lon_inc)
 
+        if self.n_vars == 1:
+            if self.all_lats.shape[0] != self.train.shape[0]:
+                 raise ValueError("Number of latitude grid points from domain boundaries doesn't equal that in training data.")
+            if self.all_lons.shape[0] != self.train.shape[1]:
+                 raise ValueError("Number of longitude grid points from domain boundaries doesn't equal that in training data.")
+
+            if self.all_lats.shape[0] != self.forecast.shape[0]:
+                 raise ValueError("Number of latitude grid points from domain boundaries doesn't equal that in forecast data.")
+            if self.all_lons.shape[0] != self.forecast.shape[1]:
+                 raise ValueError("Number of longitude grid points from domain boundaries doesn't equal that in forecast data.")
+        else:
+            if self.all_lats.shape[0] != self.train.shape[-2]:
+                 raise ValueError("Number of latitude grid points from domain boundaries doesn't equal that in training data.")
+            if self.all_lons.shape[0] != self.train.shape[-1]:
+                 raise ValueError("Number of longitude grid points from domain boundaries doesn't equal that in training data.")
+
+            if self.all_lats.shape[0] != self.forecast.shape[-2]:
+                 raise ValueError("Number of latitude grid points from domain boundaries doesn't equal that in forecast data.")
+            if self.all_lons.shape[0] != self.forecast.shape[-1]:
+                 raise ValueError("Number of longitude grid points from domain boundaries doesn't equal that in forecast data.")
+
         # --- If we're dealing with a point forecast, we will find the closest lat/lon grid points to the fcst point.
         if len(forecast_lats) > 1:
             self.closest_lat = self.all_lats[find_nearest_idx(self.all_lats[:], self.forecast_lats[0])[0]]
             self.closest_lon = self.all_lons[find_nearest_idx(self.all_lons[:], self.forecast_lons[0])[0]]
+        # --- If not, we want to find a starting/stopping lat/lon indices to generate a domain-based forecast
         else:
             self.forecast_lats = forecast_lats
             self.forecast_lons = forecast_lons
+            self.start_lat_idx = np.where(forecast_lats[0] == all_lats)[0]
+            self.start_lon_idx = np.where(forecast_lons[0] == all_lons)[0]
+            self.stop_lat_idx = np.where(forecast_lats[1] == all_lats)[0]
+            self.stop_lon_idx = np.where(forecast_lons[1] == all_lons)[0]
 
         # --- Great, it passed! Let's add in some more info for our own edification
         self.lat_bounds = lat_bounds
@@ -103,26 +131,29 @@ class SFAnalog(object):
 
     def __repr__(self):
         return "<Analog(grid_window={}, comp_method={}, field_weights={},lat_bounds={}, lon_bounds={}, forecast_lats={}, forecast_lons={}, lat_inc={}, lon_inc={})>".format(
-                             self.storm_id,self.name, self.tc_status, self.datetime, self.latitude, self.longitude,
-                             self.tc_type)
+                             self.grid_window,self.comp_method, self.field_weights, self.lat_bounds,
+                             self.lon_bounds, self.forecast_lats, self.forecast_lons, self.lat_inc, self.lon_inc)
 
 
     def find_analogs(self):
         """
         Used to find analogs for a single forecast domain.
 
-        returns:
-            analog_idxs - indices of closest analogs, from best pattern match to worst.
+        :return analog_idxs:
+            NumPy array, indices of closest analogs, from best pattern match to worst.
         """
 
         # --- Pre-generating analog indices array, this should be faster.
         if len(self.fcstLats) <= 1:
             analog_idxs = np.ones(self.train.shape[0]) * -9999.9
         else:
+            # --- Make analog indices array first, then fill it in
             if self.n_vars == 1:
                 analog_idxs = np.ones(self.train.shape) * -9999.9
             else:
                 analog_idxs = np.ones((self.train.shape[0], self.train.shape[2], self.train.shape[3])) * -9999.9
+
+            # --- Now, find closest analogs...
 
         # --- Ok, let's do this...
         if self.point_fcst:
--- py-analog/analog/comp_funcs.py
+++ py-analog/analog/comp_funcs.py
@@ -1,16 +1,66 @@
 #!/usr/bin/env python
 
 import numpy as np
-import numba as nb
+from scipy.stats import rankdata as rd
+from numba import autojit
 
-
-@jit('void(f8[:,:],f8[:,:],u8[:],u8,u8)')
-def _rmse_analog_point(train,forecast,grid_window,):
+@autojit
+def _rank_analog_grid(in_array,out_array,i_start,i_stop,j_start,j_stop, grid_window):
     """
     Function to find analogous dates based on root mean square error.
-    :param train:
-    :param forecast:
-    :param grid_window:
-    :return:
+    :param array:
+        Some 2-d numpy array
+    :return self:
     """
+    shp1 = (in_array.shape[0]-1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    shp2 = (1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    ranked_array = np.ones((in_array.shape))*-9999
+    # --- First, rank data at each array in relevant domain
+    for i in range(i_start-grid_window,i_stop+grid_window+1,1): # --- latitudes
+        for j in range(j_start-grid_window,j_stop+grid_window+1,1): # --- longitudes
+            ranked_array[:,i,j] = rd(in_array[:,i,j],method='average')
+    # --- Now find differences at each forecast grid point and local-domain
+    for i in range(i_start,i_stop+1,1): # --- latitudes
+        for j in range(j_start,j_stop+1,1): # --- longitudes
+            #temp_array[:,:] = in_array[:,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(temp_array.shape)
+            out_array[:,i,j] = np.argsort(np.sum(ranked_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
+                                      -ranked_array[-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2),
+                                      axis=1,dtype=np.int))
+    return out_array
 
+
+@autojit
+def _rmse_analog_grid(in_array,out_array,i_start,i_stop,j_start,j_stop, grid_window):
+    """
+    Function to find analogous dates based on root mean square error.
+    :param array:
+        Some 2-d numpy array
+    :return self:
+    """
+    shp1 = (in_array.shape[0]-1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    shp2 = (1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    # --- Now find differences at each forecast grid point and local-domain
+    for i in range(i_start,i_stop+1,1): # --- latitudes
+        for j in range(j_start,j_stop+1,1): # --- longitudes
+            out_array[:,i,j] = np.argsort(np.sqrt(np.mean((in_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
+                                      -in_array[-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2))**2,
+                                      axis=1,dtype=np.float),dtype=np.float))
+    return out_array
+
+@autojit
+def _mae_analog_grid(in_array,out_array,i_start,i_stop,j_start,j_stop, grid_window):
+    """
+    Function to find analogous dates based on root mean square error.
+    :param array:
+        Some 2-d numpy array
+    :return self:
+    """
+    shp1 = (in_array.shape[0]-1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    shp2 = (1,(((grid_window*2)+1)*((grid_window*2)+1)))
+    # --- Now find differences at each forecast grid point and local-domain
+    for i in range(i_start,i_stop+1,1): # --- latitudes
+        for j in range(j_start,j_stop+1,1): # --- longitudes
+            out_array[:,i,j] = np.argsort(np.mean(np.abs(in_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
+                                      -in_array[-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2)),
+                                      axis=1,dtype=np.float))
+    return out_array
