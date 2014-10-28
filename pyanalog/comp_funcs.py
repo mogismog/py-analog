9f43a12b93f0be484c4f2822d4da7e5def50a3e9
Francisco Alvarez <falvare6@slu.edu>
2014-10-28 10:26:50 -0600

    Fixed functions
    Fixed a couple more bugs, surprise!

--- pyanalog/comp_funcs.py
+++ pyanalog/comp_funcs.py
@@ -27,7 +27,7 @@ def _rank_analog_grid(train,forecast,out_array,i_start,i_stop,j_start,j_stop, gr
     # --- Now find differences at each forecast grid point and local-domain
     for i in range(i_start,i_stop+1,1): # --- latitudes
         for j in range(j_start,j_stop+1,1): # --- longitudes
-            out_array[:,i,j] = (np.sum(np.abs(ranked_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
+            out_array[:,i,j] = (np.sum(np.absolute(ranked_array[:-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp1)
                                       -ranked_array[-1,i-grid_window:i+grid_window+1,j-grid_window:j+grid_window+1].reshape(shp2)),
                                       axis=1))
     return out_array
@@ -81,8 +81,8 @@ def argsort_analogs(analog_array,i_start,i_stop,j_start,j_stop):
     # --- Now find differences at each forecast grid point and local-domain
     for i in range(i_start,i_stop+1,1): # --- latitudes
         for j in range(j_start,j_stop+1,1): # --- longitudes
-            out_array[:,i,j] = np.argsort(analog_array[:,i,j])
-    return analog_array
+            out_array[:,i,j] = rd(analog_array[:,i,j],method='ordinal')
+    return out_array
 
 @autojit
 def interp_proba(distances,events,i_start,i_stop,j_start,j_stop,pct_samps):
@@ -99,18 +99,18 @@ def interp_proba(distances,events,i_start,i_stop,j_start,j_stop,pct_samps):
             temp_events = events[:,i,j]
             # --- First, find maximum distance based on % of samples used
             # --- scale distances first
-            #scaled_dists = np.ones(distances.shape[:2])
-            #for v in range(distances.shape[0]):
-            #    scaled_dists[v,:] = (distances[v,:,i,j].T - np.min(distances[v,:,i,j]))/(np.max(distances[v,:,i,j]) - np.min(distances[v,:,i,j]))
-            #all_distances = np.sqrt(np.sum(scaled_dists.T**2,axis=0)) # --- Euclidean distance from "perfect" match
-            #max_rad = np.percentile(all_distances,pct_samps*100.) # --- max distance
-            #distance_indices = np.where(all_distances <= max_rad)[0]
-            #weights = ((max_rad - all_distances[distance_indices])/(max_rad*all_distances[distance_indices]))**2
+            scaled_dists = np.ones(distances.shape[:2])
+            for v in range(distances.shape[0]):
+                scaled_dists[v,:] = (distances[v,:,i,j].T - np.min(distances[v,:,i,j]))/(np.max(distances[v,:,i,j]) - np.min(distances[v,:,i,j]))
+            all_distances = np.sqrt(np.sum(scaled_dists.T**2,axis=0)) # --- Euclidean distance from "perfect" match
+            max_rad = np.percentile(all_distances,pct_samps*100.) # --- max distance
+            distance_indices = np.where(all_distances <= max_rad)[0]
+            weights = ((max_rad - all_distances[distance_indices])/(max_rad*all_distances[distance_indices]))**2
             # --- set up RBF function
             #print events[:,i,j].max()
-            rbfi = Rbf(distances[0,:,i,j],distances[1,:,i,j],distances[2,:,i,j],events[:,i,j])
-            #probs[i,j] = np.sum(weights*temp_events[distance_indices])/np.sum(weights)
-            probs[i,j] = rbfi(0,0,0) # --- perfect forecast
+            probs[i,j] = np.sum(weights*temp_events[distance_indices])/np.sum(weights)
+            #rbfi = Rbf(distances[0,:,i,j],distances[1,:,i,j],distances[2,:,i,j],events[:,i,j])
+            #probs[i,j] = rbfi(0,0,0) # --- perfect forecast
     return probs
 
 @autojit
@@ -128,6 +128,9 @@ def gen_proba(distances,events,i_start,i_stop,j_start,j_stop,n_analogs):
             temp_events = events[:,i,j]
             # --- First, find maximum distance based on % of samples used
             # --- scale distances first
-            distance_indices = np.where(distances[:,i,j] < n_analogs)[0]
+            if n_analogs < 1:
+                distance_indices = np.where(distances[:,i,j] < np.ceil(distances[:,i,j].shape[0]*n_analogs))[0]
+            elif n_analogs >= 1:
+                distance_indices = np.where(distances[:,i,j] < n_analogs)[0]
             probs[i,j] = np.sum(temp_events[distance_indices])/len(distance_indices)
     return probs
